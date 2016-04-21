d85646b378aa29e1da29801201e6b83b89c6e574
Michal Lichwa <michal@strangeflock.com>
2016-04-21 10:44:23 -0500

    Initial Commit

---
+++ CollectionViewPinchZoom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewPinchZoom
+//
+//  Created by Michal Lichwa on 4/21/16.
+//  Copyright © 2016 Michal Lichwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
